7136a7f94ede398a00e965fbd4c33626a17123d9
Ludovit Scholtz <ludovit.scholtz@aaaauto.cz>
2022-04-05 16:17:36 +0200

    update auth, allow non auth user as 200 response with role public

--- HasuraAlgorandAuthWebHook/Controllers/AuthController.cs
+++ HasuraAlgorandAuthWebHook/Controllers/AuthController.cs
@@ -18,11 +18,18 @@ namespace HasuraAlgorandAuthWebHook.Controllers
         [HttpGet("webhook")]
         public ActionResult<Model.WebhookResponse> Get()
         {
-            var ret = new Model.WebhookResponse() { UserId = User.Identity.Name };
-            var exp = User.Claims.FirstOrDefault(c=>c.Type == "exp");
-            
-            if (!string.IsNullOrEmpty(exp?.Value)) if (long.TryParse(exp.Value, out var time)) ret.Expires = DateTimeOffset.FromUnixTimeSeconds(time);
-            return Ok(ret);
+            if (string.IsNullOrEmpty(User?.Identity?.Name))
+            {
+                var ret = new Model.WebhookResponse() { UserId = "", Role = "public" };
+                return Ok(ret);
+            }
+            else
+            {
+                var ret = new Model.WebhookResponse() { UserId = User.Identity.Name };
+                var exp = User.Claims.FirstOrDefault(c => c.Type == "exp");
+                if (!string.IsNullOrEmpty(exp?.Value)) if (long.TryParse(exp.Value, out var time)) ret.Expires = DateTimeOffset.FromUnixTimeSeconds(time);
+                return Ok(ret);
+            }
         }
     }
 }
