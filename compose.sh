2504cc5c437d1927c4ff86678f3fd610e53024a3
Ludovit Scholtz <ludko2@gmail.com>
2022-06-12 17:17:45 +0200

    release new version

--- compose.sh
+++ compose.sh
@@ -1,4 +1,4 @@
-ver=1.0.0
+ver=1.2022.6.12
 docker build -t "scholtz2/hasura-algorand-auth-web-hook:$ver-stable" -f HasuraAlgorandAuthWebHook/Dockerfile  ./
 docker push "scholtz2/hasura-algorand-auth-web-hook:$ver-stable"
 echo "Image: scholtz2/hasura-algorand-auth-web-hook:$ver-stable"
